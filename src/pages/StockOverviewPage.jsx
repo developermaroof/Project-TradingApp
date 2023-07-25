4001a301ab24988bf2f05db165106b89debefd60
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2023-07-25 14:45:45 +0500

    updated

--- src/pages/StockOverviewPage.jsx
+++ src/pages/StockOverviewPage.jsx
@@ -1,9 +1,13 @@
 import { AutoComplete } from "../components/AutoComplete";
 import { StockList } from "../components/StockList";
+import trading from "../Images/Trading.png";
 
 export const StockOverviewPage = () => {
   return (
     <div>
+      <div className="text-center">
+        <img src={trading} alt="trading img" />
+      </div>
       <AutoComplete />
       <StockList />
     </div>
